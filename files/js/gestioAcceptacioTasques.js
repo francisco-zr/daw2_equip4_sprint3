f6a42f768ee8ebb2631dc989443d1cccd84d4936
samuel-lara <samuellara@iesmontsia.org>
2023-01-09 21:26:37 +0100

    falta rellenar los campos con false al iniciar documento

--- files/js/gestioAcceptacioTasques.js
+++ files/js/gestioAcceptacioTasques.js
@@ -1,5 +1,6 @@
 var tareas = []; //Array donde guardamos todas las tareas aceptadas
 
+
 /** evento de la BootStrap Table */
 window.operateEvents = {
   /** Eventos que se ejecutan cuando se hace click en el elemento con la clase form-check-input */
@@ -8,10 +9,6 @@ window.operateEvents = {
     var gestion = document.getElementById(
       "gestion-tareas-" + row.id_recommendation
     ); //recuperamos el desplegable mediante el DOM
-
-    if(checkeado == false){
-      console.log("soy false");
-    }
 
     if (checkeado == true) {
       document.getElementById( "gestion-tareas-" + row.id_recommendation + "").disabled = false; //habilitamos el desplegable para poder seleccionar quien gestiona la tarea
@@ -30,12 +27,7 @@ window.operateEvents = {
 
           tareas.forEach(e => {
             //Comprueba si el id de la tarea que estamos trabajando es igual que vamos a insertar, si es igual borrará todos los anteriores y creará uno nuevo con la nueva gestión seleccionada
-            /*if(tareas.id_recommendation == e.id_recommendation){
-              tareas.splice(e.id_recommendation, 1, {
-                id: row.id_recommendation,
-                accepted: true,
-                gestion: gestion.value,
-              });*/
+          
               if(e.id == row.id_recommendation){
                 console.log("El índice es" + contador);
 
@@ -102,45 +94,13 @@ window.operateEvents = {
 };
 
 
-$(document).ready(function () {
-  $("#enviar-tareas").click(function () {
-    /** poner aqui el envio del json al php 
-                   const options = {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(tareas)
-                   } 
-
-                    fetch('../public/enviarTasquesAcceptadesJSON.php', options)
-                        .then(response => response.json())
-                        .then(data => console.log(tareas))*/
-
-
-
-    
-    // Crea un nuevo objeto XMLHttpRequest
-    var request = new XMLHttpRequest();
-
-    // Configura la solicitud
-    request.open("POST", "../public/enviarTasquesAcceptadesJSON.php", true);
-
-    // Establece la cabecera para indicar que se enviará una cadena de texto
-    request.setRequestHeader("Content-Type", "application/json");
-
-    // Envía la solicitud con el array como carga
-    request.send(JSON.stringify(tareas));
-    console.log(tareas);
-  });
-});
-
-
 function operateFormatter(value, row, index) {
+  
+
   return `
                 <div class="right">
                 <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault-row-${row.id_recommendation}" value="#pasar-el-checked-true-o-false">
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault-row-${row.id_recommendation}" value="false">
                 <input id="valor-escondido-${row.id_recommendation}" value="${row.id_recommendation}" hidden>
                 <select id="gestion-tareas-${row.id_recommendation}" name="gestion-tareas" class="gestion-tareas" disabled>
                     <option value="">Seleccionar una opción</option>
@@ -150,3 +110,70 @@ function operateFormatter(value, row, index) {
                 </div>
             `;
 }
+
+
+
+/** RELLENAR EL ARRAY CON DATOS VACIOS AL INCIAR EL DOCUMENTO */
+$(document).ready(function () {
+
+const largoTabla = $("#table").bootstrapTable('getData').length;
+//const largoTabla = [12, 13, 67];
+console.log(largoTabla);
+
+for(let i = 1; i <= largoTabla ; i++){
+  console.log(i);
+}
+
+
+
+var selector = $("#flexSwitchCheckDefault-row-1");
+console.log(selector);
+
+//Si los selectores no estan chequeados
+if (!selector.is(":checked")) {
+  contador = 1;
+  
+  tareas.push({
+    id: contador,
+    accepted: false,
+    gestion: "",
+  });
+  console.log("El elemento no está seleccionado")
+  console.log(tareas)
+}
+  
+  
+ 
+   
+ 
+   // $("#enviar-tareas").click(function () {
+   //   /** poner aqui el envio del json al php 
+   //                  const options = {
+   //                       method: 'POST',
+   //                       headers: {
+   //                           'Content-Type': 'application/json'
+   //                       },
+   //                       body: JSON.stringify(tareas)
+   //                  } 
+ 
+   //                   fetch('../public/enviarTasquesAcceptadesJSON.php', options)
+   //                       .then(response => response.json())
+   //                       .then(data => console.log(tareas))*/
+ 
+ 
+ 
+     
+   //   // Crea un nuevo objeto XMLHttpRequest
+   //   var request = new XMLHttpRequest();
+ 
+   //   // Configura la solicitud
+   //   request.open("POST", "../public/enviarTasquesAcceptadesJSON.php", true);
+ 
+   //   // Establece la cabecera para indicar que se enviará una cadena de texto
+   //   request.setRequestHeader("Content-Type", "application/json");
+ 
+   //   // Envía la solicitud con el array como carga
+   //   request.send(JSON.stringify(tareas));
+   //   console.log(tareas);
+   // });
+ });
