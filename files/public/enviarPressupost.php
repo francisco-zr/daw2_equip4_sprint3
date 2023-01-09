ac38d1eca7d32326bc07da16747034fe77ff4955
Julen Martinez <julenmartinez@iesmontsia.org>
2023-01-09 19:40:04 +0100

    enviar pressupost

--- files/public/enviarPressupost.php
+++ files/public/enviarPressupost.php
@@ -4,11 +4,12 @@ if (isset($_GET['id_presupuesto'])) {
     $id = $_GET['id_presupuesto'];
   }
 $presupuesto = new Presupost($id);
-if ($_POST == $id_task)
+
 foreach ($_POST as $id_task => $row) {
     //echo $id_task . '-' . $row . '<br>';
     $presupuesto->afegirPreuTasca($row, substr($id_task, 0, -1));
 };
+
 foreach ($_POST as $start_date => $row) {
   $presupuesto->actualitzarData($row, substr($start_date, 0, -1));
 };
