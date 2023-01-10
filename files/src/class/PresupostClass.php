6fff5274838fbc5a0d49dce2830c44900d4782ef
Julen Martinez <julenmartinez@iesmontsia.org>
2023-01-10 17:43:52 +0100

    formulari tasques canvis

--- files/src/class/PresupostClass.php
+++ files/src/class/PresupostClass.php
@@ -167,7 +167,7 @@ class Presupost
     {
         include '../config/connexioBDD.php';
 
-        $query = "SELECT tasks.name_task, tasks.id_task, task_budget.price, tasks.start_date, tasks.final_date FROM `tasks` LEFT JOIN task_budget ON task_budget.id_task = tasks.id_task INNER JOIN recommendations ON tasks.id_recommendation = recommendations.id_recommendation INNER JOIN questionnaries ON tasks.id_questionary = questionnaries.id_questionary WHERE questionnaries.id_questionary = $this->presupost AND tasks.accepted = 1;";
+        $query = "SELECT recommendations.name_recommendation, tasks.id_task, tasks.price, tasks.start_date, tasks.final_date FROM `tasks` INNER JOIN recommendations ON tasks.id_recommendation = recommendations.id_recommendation INNER JOIN questionnaries ON tasks.id_questionary = questionnaries.id_questionary WHERE questionnaries.id_questionary = $this->presupost AND tasks.accepted = 1;";
         return $connexioDB->query($query);
     }
 
